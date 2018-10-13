785e8168a2880fedb836be90b2e3b99ae05270f2
ayoupov <ayoupov@gmail.com>
2018-10-13 18:12:24 +0300

    remove the key

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -1,5 +1,4 @@
 var firebaseConfig = {
-//  apiKey: "AIzaSyDmI6E2lyYr55NA9htDAM0MQ31MY5RPvKc",
   authDomain: "phi-hydro-simulation.firebaseapp.com",
   databaseURL: "https://phi-hydro-simulation.firebaseio.com/"
 };
